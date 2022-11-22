0c58bf43f59d883e430f82e56642e0c60c0c6627
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2022-11-22 18:55:51 +0000

    master -> Update configs

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ new_devlog_entry:
 	python3 meta/scripts/new_devlog_entry.py
 
 run:
-	python3 src/main.py
+	python3 file_sorter/main.py
 
 test:
 	python3 -m unittest discover -v -s file_sorter/ -t .
--- file_sorter/configs.py
+++ file_sorter/configs.py
@@ -2,17 +2,19 @@ import json
 
 
 class Configutations:
+    configuration = None
+
     def __init__(self, settings_file) -> None:
         f = open(settings_file)
-        self.data = json.load(f)
+        Configutations.configuration = json.load(f)
         f.close()
 
-    def get_logging_level(self) -> str:
-        return self.data["logging"]["level"]
+    def get_logging_level() -> str:
+        return Configutations.configuration["logging"]["level"]
 
-    def get_logging_file(self) -> str:
-        return self.data["logging"]["file"]
+    def get_logging_file() -> str:
+        return Configutations.configuration["logging"]["file"]
 
 
-def create_configuration(configuration_file: str) -> Configutations:
+def load_configuration(configuration_file: str) -> Configutations:
     return Configutations(configuration_file)
