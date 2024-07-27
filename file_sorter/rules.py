666db61f8b397454b6545b45006033e540b0cf8e
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2024-07-27 21:31:57 +0100

    Add DeleteZipFileRule

--- file_sorter/rules.py
+++ file_sorter/rules.py
@@ -17,7 +17,11 @@ class DeleteDebFileRule(AbstractRule):
         if file.file_type == "deb":
             queue.queue_action(DeleteFileAction(file))
 
+class DeleteZipFileRule(AbstractRule):
+    def invokate(self, file: File, queue: FileActionQueue) -> None:
+        if file.file_type == "zip":
+            queue.queue_action(DeleteFileAction(file))
+
 class PrintName(AbstractRule):
     def invokate(self, file: File, queue: FileActionQueue):
         print(file.name)
-
