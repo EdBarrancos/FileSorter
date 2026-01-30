9772ccf584786470bfc8eda7080689c3cdb67e3f
Eduardo Barrancos <eduardobarrancos@premium-minds.com>
2026-01-30 14:54:47 +0000

    Remove prints

--- src/FileSorter.jl
+++ src/FileSorter.jl
@@ -11,7 +11,6 @@ include("customRules/CustomRulesModule.jl")
 using .CustomRules: dispatch
 
 function __init__()
-    tstart = time()
     input = parseInput(ARGS)
     if isnothing(input)
         exit()
@@ -20,9 +19,7 @@ function __init__()
         @error "Provided target is not a directory"
         return
     end
-    tinput = time()
-    print("Input parsed: ")
-    println(tinput - tstart)
+
     app::FileSorterApp = FileSorterApp()
     if length(input) > 1
         foreach(rule -> length(rule) == 1 ?
@@ -30,23 +27,8 @@ function __init__()
                         hook!(app, dispatch(rule[begin], rule[2:end]...)), input[2:end]...)
     end
 
-    thook = time()
-    print("Rules hooked: ")
-    println(thook - tstart)
-
     process(app, input[begin])
 
-    tprocessed = time()
-    print("Rules processed: ")
-    println(tprocessed - tstart)
-
     foreach(item -> execute(item), app.actionQueue.items)
-
-    tactionQueue = time()
-    print("Action queue: ")
-    println(tactionQueue - tstart)
-    tend = time()
-    println(tend - tstart)
 end
-
 end
